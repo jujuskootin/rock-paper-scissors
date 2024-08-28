71f080c114cedac164e26ad67c2fd9174302fcb1
Julian Rubio <iamjulian54@gmail.com>
2024-08-28 18:36:12 -0500

    Create logic that generates rock, paper, or scissors
    Created a funtion to get a randomized rock paper scissors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const comChoice = Math.floor(Math.random() * 3) 
+function getComputerChoice() {
+  if (comChoice === 0)
+    console.log('Rock')
+  else if (comChoice === 1)
+    console.log('Paper')
+  else if (comChoice === 2)
+    console.log('Scissors')
+  }
+  console.log(getComputerChoice(comChoice))
+
