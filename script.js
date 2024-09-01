285957f58282dd7ea0e86147e3099ff6cce9ebfa
Julian Rubio <iamjulian54@gmail.com>
2024-09-01 14:47:15 -0500

    Write logic to play a round of the game
    Wrote the logic that compares the choices given and returns a winner

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const comChoice = Math.floor(Math.random() * 3) 
+const computerChoice = Math.floor(Math.random() * 3) 
 function getComputerChoice() {
-  if (comChoice === 0)
-    console.log('rock')
-  else if (comChoice === 1)
-    console.log('paper')
-  else if (comChoice === 2)
-    console.log('scissors')
+  if (computerChoice === 0)
+    return('rock')
+  else if (computerChoice === 1)
+    return('paper')
+  else if (computerChoice === 2)
+    return('scissors')
   }
-  console.log(getComputerChoice(comChoice))
+console.log(getComputerChoice(computerChoice))
 
-  let humChoice = prompt('Make your choice').toLowerCase()
+  let humanChoice = prompt('Make your choice').toLowerCase()
 function getHumanChoice() {
-    return(humChoice)
+    return(humanChoice)
     
 }
-console.log(getHumanChoice(humChoice))
+console.log(getHumanChoice(humanChoice))
 
 let humanScore = 0
-
 let computerScore = 0
+
+
+function playRound(humanChoice, computerChoice) {
+if (humanChoice === 'rock' && computerChoice === 'scissors')
+  console.log('You win!')
+else if (humanChoice === 'rock' && computerChoice === 'paper')
+  console.log('You lose!')
+else if (humanChoice === 'paper' && computerChoice === 'rock')
+  console.log('You win!')
+else if (humanChoice === 'paper' && computerChoice === 'scissors')
+  console.log('You lose!')
+else if (humanChoice === 'scissors' && computerChoice === 'paper')
+  console.log('You win!')
+else if (humanChoice === 'scissors' && computerChoice === 'rock')
+  console.log('You lose!')
+else if (humanChoice == computerChoice)
+  console.log('It\'s a tie!')
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
+
